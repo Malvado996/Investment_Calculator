c79c1fe28b01aed885a27a85aa9fb57b6bfabc0b
Philip Rodrigues <philip.rodrigues.js@gmail.com>
2025-12-27 10:42:51 -0500

    Results Component

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,4 @@
-import { calculateInvestmentResults } from '../util/investment';
+import { calculateInvestmentResults, formatter } from '../util/investment';
 
 import type { InvestmentInput } from '../App'
 
@@ -15,11 +15,33 @@ const Results: React.FC<ResultsProps> = ({ input }) => {
     console.log(resultsData);
 
     return (
-        <section id='results'>
-            <h2>Results...</h2>
-            <p>Investment data received:</p>
-            <pre>{JSON.stringify(input, null, 2)}</pre>
-        </section>
+        <table id='result'>
+            <thead>
+                <tr>
+                    <th>Year</th>
+                    <th>Investment Value</th>
+                    <th>Interest (Year)</th>
+                    <th>Total Interest</th>
+                    <th>Invested Capital</th>
+                </tr>
+            </thead>
+            <tbody>
+                {resultsData.map(yearData => {
+
+                    const totalInterest = yearData.valueEndOfYear - yearData.annualInvestment * yearData.year
+                    const initialInvestment = resultsData[0].valueEndOfYear - resultsData[0].interest - resultsData[0].annualInvestment
+                    const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
+
+                    return <tr key={yearData.year}>
+                        <td>{yearData.year}</td>
+                        <td>{formatter.format(yearData.valueEndOfYear)}</td>
+                        <td>{formatter.format(yearData.interest)}</td>
+                        <td>{formatter.format(totalInterest)}</td>
+                        <td>{formatter.format(totalAmountInvested)}</td>
+                    </tr>
+                })}
+            </tbody>
+        </table>
     )
 }
 
